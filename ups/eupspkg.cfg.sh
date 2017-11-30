6be37da467ad745d26210d3d20ea45c4c9e07b5d
Tim Jenness <tjenness@lsst.org>
2017-11-30 13:15:34 -0700

    Workaround problem with setuptools_scm interpreting tags

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -1,2 +1,14 @@
 # Eupspkg config file. Source by 'eupspkg'
 TAP_PACKAGE=1
+
+build() {
+  mv .git .git-save
+  default_build
+  mv .git-save .git
+}
+
+install() {
+  mv .git .git-save
+  default_install
+  mv .git-save .git
+}
